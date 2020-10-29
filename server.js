9511391468813b9db1f850ebe7a2d961d5380653
Anna Kaser <kaserann96@gmail.com>
2020-10-29 10:55:16 -0600

    login logic

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
